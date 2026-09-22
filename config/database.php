119eb9fcb934b465fe5905cb893e30ee9471ad72
Marcell Matthew Loemanto <marcell.loemanto@gmail.com>
2026-09-22 11:09:04 +0700

    Use Neon direct connection for migrations

--- config/database.php
+++ config/database.php
@@ -17,7 +17,9 @@ return [
     |
     */
 
-    'default' => env('POSTGRES_URL') ? 'pgsql' : env('DB_CONNECTION', 'sqlite'),
+    'default' => (env('POSTGRES_DATABASE_URL_UNPOOLED') || env('POSTGRES_URL'))
+        ? 'pgsql'
+        : env('DB_CONNECTION', 'sqlite'),
 
     /*
     |--------------------------------------------------------------------------
@@ -86,7 +88,7 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DB_URL', env('POSTGRES_URL')),
+            'url' => env('DB_URL', env('POSTGRES_DATABASE_URL_UNPOOLED', env('POSTGRES_URL'))),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'laravel'),
